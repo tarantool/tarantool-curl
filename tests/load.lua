708d6c7a8ac91cd0950ab2641ca1bf45c23f7abd
dedok <dedok.mad@gmail.com>
2017-02-02 03:31:23 +0300

    update tests

--- tests/load.lua
+++ tests/load.lua
@@ -8,7 +8,7 @@ package.preload['curl.driver'] = 'curl/driver.so'
 --
 
 -- Includes
-box.cfg { }
+box.cfg { slab_alloc_factor = 0.1 }
 
 local curl  = require('curl')
 local fiber = require('fiber')
@@ -42,27 +42,27 @@ for i = 1, num do
 
   local obj = curls[i]
 
-  fiber.create(function()
-    for j = 1, 100 do
-      pcall(
-        function()
-          obj.http:post(obj.url, obj.body,
-                        {headers = obj.headers,
-                         keepalive_idle = 30,
-                         keepalive_interval = 60,
-                         connect_timeout = obj.connect_timeout,
-                         read_timeout = obj.read_timeout,
-                         dns_cache_timeout = obj.dns_cache_timeout, })
-          obj.http:get(obj.url,
-                        {headers = obj.headers,
-                         keepalive_idle = 30,
-                         keepalive_interval = 60,
-                         connect_timeout = obj.connect_timeout,
-                         read_timeout = obj.read_timeout,
-                         dns_cache_timeout = obj.dns_cache_timeout, })
-        end)
-    end
-  end )
+  for j = 1, 100 do
+      fiber.create(function()
+          pcall(
+            function()
+              obj.http:post(obj.url, obj.body,
+                            {headers = obj.headers,
+                             keepalive_idle = 30,
+                             keepalive_interval = 60,
+                             connect_timeout = obj.connect_timeout,
+                             read_timeout = obj.read_timeout,
+                             dns_cache_timeout = obj.dns_cache_timeout, })
+              obj.http:get(obj.url,
+                            {headers = obj.headers,
+                             keepalive_idle = 30,
+                             keepalive_interval = 60,
+                             connect_timeout = obj.connect_timeout,
+                             read_timeout = obj.read_timeout,
+                             dns_cache_timeout = obj.dns_cache_timeout, })
+            end)
+      end )
+  end
 end
 
 -- Join test
@@ -82,20 +82,19 @@ fiber.create(function()
 
       local obj = curls[i]
 
-      print(yaml.encode(obj.http:stat()))
-
-      if obj.http:stat().active_requests == 0 then
+      if obj.http ~= nil and obj.http:stat().active_requests == 0 then
         local st = obj.http:stat()
         assert(st.sockets_added == st.sockets_deleted)
         assert(st.active_requests == 0)
         assert(st.loop_calls > 0)
         obj.http:free()
         rest = rest - 1
+        curls[i].http = nil
       end
 
     end
 
-    if rest == 0 then
+    if rest <= 0 then
       break
     end
 
